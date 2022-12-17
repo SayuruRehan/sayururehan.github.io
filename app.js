f58118c76e12d714dc5e19948dab4e8ff6d7e6f5
Sayuru Rehan <42121050+SayuruRehan@users.noreply.github.com>
2022-12-18 01:42:45 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function PageTransitions(){
+
+    //button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
+            this.className += 'active-btn';
+        })
+    }
+
+    //section click active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from btns
+            sectBtns.forEach(btn => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active')
+
+            //hide sections
+            sections.forEach(section => {
+                section.classList.remove('active');
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+PageTransitions();
